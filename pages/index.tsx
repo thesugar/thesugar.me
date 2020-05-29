0a382d6b59d043c9dc2d570afea90330766e47be
ryoheisato <thesugar87@gmail.com>
2020-05-29 20:48:18 +0900

    inspect inside of .next/serverless/pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ const Home = (): JSX.Element => {
       </Head>
       <header className="header">{THESUGARME}</header>
 
-      <p>This is development env. 18:51</p>
+      <p>This is development env. 20:47</p>
       {selfIntroduction}
 
       <section>
