99a75532dac307843eeb3bcbfbbb7857f5e426ea
ryoheisato <thesugar87@gmail.com>
2020-05-31 23:18:36 +0900

    refix

--- test.ts
+++ test.ts
@@ -5,5 +5,5 @@ export const writeFileFunc = () => {
     console.log("inside writeFileFunc")
     console.log(process.cwd()) // `var/task`
     console.log(__dirname) // `/`
-    fs.writeFileSync(path.join(process.cwd(), '.next','serverless','pages'), `{"foo": "hoge", "bar": "fuga"}`)
+    fs.writeFileSync(path.join(process.cwd(), '.next','serverless','pages', 'newfile.json'), `{"foo": "hoge", "bar": "fuga"}`)
 }
