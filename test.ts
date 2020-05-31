63ab5d10b122a4e095a2a73dfbbaa374aa764fba
ryoheisato <thesugar87@gmail.com>
2020-05-31 23:03:11 +0900

    fix again

--- test.ts
+++ test.ts
@@ -2,5 +2,7 @@ import fs from 'fs'
 
 export const writeFileFunc = () => {
     console.log("inside writeFileFunc")
+    console.log(process.cwd())
+    console.log(__dirname)
     fs.writeFileSync('./datasrc/undefined.json', `{"foo": "hoge", "bar": "fuga"}`)
 }
