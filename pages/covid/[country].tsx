86c4d59f3a9f6d6ca847e53c8572cc42fd55936b
ryoheisato <thesugar87@gmail.com>
2020-10-16 16:50:56 +0900

    added Chart.js

--- pages/covid/[country].tsx
+++ pages/covid/[country].tsx
@@ -1,8 +1,8 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
 import { siteTitle } from '../../components/sugar.config'
 import THESUGARME from '../../components/ThesugarMe'
+import { Line } from 'react-chartjs-2'
 
 type Data = {
   Country: string
@@ -22,6 +22,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       `https://api.covid19api.com/dayone/country/${country}/status/confirmed/live`
     )
     const json: Data[] = await res.json()
+    const now = new Date().toLocaleTimeString()
 
     if (res.status !== 200) {
       console.error(json)
@@ -37,8 +38,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
           country: json[0].Country,
           status: 'ok',
           result: resultArray,
+          now
         },
-        revalidate: 30,
+        revalidate: 60,
       }
     }
   } catch (e) {
@@ -71,40 +73,112 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
+const data = (country: string, date: string[], cases: number[]) => {
+  return {
+    labels: date,
+    datasets: [
+      {
+        type: 'line',
+        label: country,
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'round',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'square',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#eee',
+        pointBorderWidth: 1,
+        pointHoverRadius: 8,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 1,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: cases,
+      },
+    ],
+  }
+}
+
+const chartOption = (country: string) => {
+  return {
+    title: {
+      display: true,
+      text: country,
+    },
+    tooltips: {
+      mode: 'label',
+    },
+    responsive: false,
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [
+        {
+          stacked: true,
+        },
+      ],
+      yAxes: [
+        {
+          stacked: true,
+          scaleLabel: {
+            display: true,
+            labelString: '人',
+          },
+        },
+      ],
+    },
+  }
+}
+
 const Page = (props: {
   country: string
   status: 'ok' | 'fail'
-  result: [{ date: string; cases: string }]
+  result: [{ date: string; cases: number }]
+  now: string
 }) => {
   return (
     <div className="allContainer">
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <header className="header">
-        {THESUGARME}
-      </header>
+      <header className="header">{THESUGARME}</header>
 
       <main>
-        <h3>{props.country}
-        {props.status === 'ok' && ' の感染者推移'}
-        {props.status === 'fail' && ' のデータは取得できませんでした。'}</h3>
+        <h3>
+          {props.country}
+          {props.status === 'ok' && ' の感染者推移'}
+          {props.status === 'fail' && ' のデータは取得できませんでした。'}
+        </h3>
         <br />
-        {props.status === 'ok' &&
-          props.result.map((element) => (
-            <ul className="oneday-data" key={element.date}>
-              <li>{element.date.slice(0, 10)}</li>
-              <li>{element.cases}</li>
-            </ul>
-          ))}
+        {props.status === 'ok' && (
+          <Line
+            data={data(
+              props.country,
+              props.result.map((eachData) => eachData.date.slice(0, 10)),
+              props.result.map((eachData) => eachData.cases)
+            )}
+            width={600}
+            height={400}
+            options={chartOption(props.country)}
+          />
+        )}
+        <br />
+        <small>
+        最終更新:
+        {props.now}
+        </small>
       </main>
       <style jsx>{`
         main {
           display: grid;
           max-width: 50vw;
           justify-content: center;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-          Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
         }
 
         ul {
